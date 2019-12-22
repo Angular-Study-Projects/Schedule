a53407e84850ac093d97827655ea5595462bd8eb
lpgoulart <leonardo@roffor.com.br>
2019-12-22 01:02:56 -0300

    update

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+
+
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { TableComponent } from 'src/app/components/table/table.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TableComponent
   ],
   imports: [
+    NzTableModule,
     CommonModule,
     HomeRoutingModule
   ],
